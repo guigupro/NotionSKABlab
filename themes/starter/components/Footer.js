feee7c3583e7ccd13992aab6f2aaaf8414bd2217
guigu <51107249+guigupro@users.noreply.github.com>
2024-08-03 15:47:33 +0800

    Update Footer.js

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -148,11 +148,11 @@ export const Footer = props => {
                   <p className='text-base text-gray-7'>
                     Designed and Developed by
                     <a
-                      href='https://github.com/tangly1024/NotionNext'
+                      href='https://www.skablab.top/'
                       rel='nofollow noopner noreferrer'
                       target='_blank'
                       className='px-1 text-gray-1 hover:underline'>
-                      NotionNext {siteConfig('VERSION')}
+                      YGZheng 2024
                     </a>
                   </p>
                 </div>
